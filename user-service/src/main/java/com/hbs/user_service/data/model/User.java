8d36e043c06f80e16709c1132510657b3091053b
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-11-21 11:39:45 +0530

    update the user model class

--- user-service/src/main/java/com/hbs/user_service/data/model/User.java
+++ user-service/src/main/java/com/hbs/user_service/data/model/User.java
@@ -5,9 +5,17 @@ import lombok.Data;
 
 @Entity
 @Table(name = "tbl_user")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "user_type", discriminatorType = DiscriminatorType.STRING)
 @Data
-public class User {
+public abstract class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    private String username;
+
+    private String password;
+
+    private String email;
 }
