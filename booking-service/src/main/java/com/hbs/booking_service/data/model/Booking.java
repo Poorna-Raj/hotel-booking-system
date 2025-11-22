a81e05685dbce9bdf4f1d1c6406311946e22449f
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-11-22 17:26:51 +0530

    define the model class for the booking

---
+++ booking-service/src/main/java/com/hbs/booking_service/data/model/Booking.java
@@ -0,0 +1,46 @@
+package com.hbs.booking_service.data.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tbl_booking")
+@Data
+public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "room_id", nullable = false)
+    private long roomId;
+
+    @Column(name = "created_by", nullable = false)
+    private long createdBy;
+
+    @Column(name = "check_in", nullable = false)
+    private LocalDateTime checkIn;
+
+    @Column(name = "check_out", nullable = false)
+    private LocalDateTime checkOut;
+
+    @Column(name = "booking_status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private BookingStatus bookingStatus;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "customer_name", nullable = false)
+    private String customerName;
+
+    @Column(name = "customer_nic", nullable = false)
+    private String customerNic;
+
+    @Column(name = "total_amount", nullable = false)
+    private double totalAmount;
+}
