5ffd2da84e0050752388e94d5900e6b8d5b3aef4
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-11-22 17:27:45 +0530

    implement the data transfer object for the booking responses

---
+++ booking-service/src/main/java/com/hbs/booking_service/data/dto/BookingResponseDto.java
@@ -0,0 +1,21 @@
+package com.hbs.booking_service.data.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+@Getter
+@Setter
+public class BookingResponseDto {
+    private long id;
+    private long roomId;
+    private long createdBy;
+    private LocalDateTime checkIn;
+    private LocalDateTime checkOut;
+    private String bookingStatus;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private String customerName;
+    private String customerNic;
+    private double totalAmount;
+}
