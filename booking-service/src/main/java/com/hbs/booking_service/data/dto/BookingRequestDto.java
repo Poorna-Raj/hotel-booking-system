7caa6b571efb92555e70a883d7683869a161efbd
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-11-24 16:40:46 +0530

    change the data types for date times

--- booking-service/src/main/java/com/hbs/booking_service/data/dto/BookingRequestDto.java
+++ booking-service/src/main/java/com/hbs/booking_service/data/dto/BookingRequestDto.java
@@ -3,14 +3,16 @@ package com.hbs.booking_service.data.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 public class BookingRequestDto {
     private long id;
     private long roomId;
     private long createdBy;
-    private String checkIn;
-    private String checkOut;
+    private LocalDateTime checkIn;
+    private LocalDateTime checkOut;
     private String bookingStatus;
     private String customerName;
     private String customerNic;
