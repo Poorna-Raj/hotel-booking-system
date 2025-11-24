9f2a8b21efbf497df709675c5ec7c41172db4674
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-11-24 16:44:05 +0530

    add validation method to check the booking status enum

--- booking-service/src/main/java/com/hbs/booking_service/service/BookingValidator.java
+++ booking-service/src/main/java/com/hbs/booking_service/service/BookingValidator.java
@@ -1,5 +1,6 @@
 package com.hbs.booking_service.service;
 
+import com.hbs.booking_service.data.model.BookingStatus;
 import com.hbs.booking_service.data.repository.BookingRepository;
 import com.hbs.booking_service.exception.BookingValidationError;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,11 +14,11 @@ public class BookingValidator {
     private BookingRepository repository;
 
     public boolean isBookingValid(LocalDateTime checkingTime, LocalDateTime checkoutTime, long roomId){
-        if(!isCheckingValid(checkingTime)){
+        if(isCheckingValid(checkingTime)){
             throw new BookingValidationError("Invalid checking time");
         }
 
-        if(!isCheckoutValid(checkoutTime,checkingTime)){
+        if(isCheckoutValid(checkoutTime, checkingTime)){
             throw new BookingValidationError("Invalid checkout time");
         }
 
@@ -29,11 +30,11 @@ public class BookingValidator {
     }
 
     public boolean isBookingValid(LocalDateTime checkingTime,LocalDateTime checkoutTime,long roomId,long bookingId){
-        if(!isCheckingValid(checkingTime)){
+        if(isCheckingValid(checkingTime)){
             throw new BookingValidationError("Invalid checking time");
         }
 
-        if(!isCheckoutValid(checkoutTime,checkingTime)){
+        if(isCheckoutValid(checkoutTime, checkingTime)){
             throw new BookingValidationError("Invalid checkout time");
         }
 
@@ -44,12 +45,21 @@ public class BookingValidator {
         return true;
     }
 
+    public boolean isBookingStatusValid(String status){
+        try{
+            BookingStatus.valueOf(status);
+            return true;
+        } catch (IllegalArgumentException ex){
+            throw new BookingValidationError("Invalid Booking Status!");
+        }
+    }
+
     private boolean isCheckingValid(LocalDateTime checking) {
-        return LocalDateTime.now().isBefore(checking);
+        return !LocalDateTime.now().isBefore(checking);
     }
 
     private boolean isCheckoutValid(LocalDateTime checkout, LocalDateTime checking){
-        return LocalDateTime.now().isBefore(checkout) && checking.isBefore(checkout);
+        return !LocalDateTime.now().isBefore(checkout) || !checking.isBefore(checkout);
     }
 
     private boolean isBookingOverlapped(LocalDateTime checking, LocalDateTime checkout, long roomId){
