59c3bd013dca5def1db280ee154d87004181a12d
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-11-24 16:29:29 +0530

    add methods to check overlapping bookings

--- booking-service/src/main/java/com/hbs/booking_service/data/repository/BookingRepository.java
+++ booking-service/src/main/java/com/hbs/booking_service/data/repository/BookingRepository.java
@@ -2,6 +2,37 @@ package com.hbs.booking_service.data.repository;
 
 import com.hbs.booking_service.data.model.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
 
 public interface BookingRepository extends JpaRepository<Booking,Long> {
+    @Query("""
+    SELECT COUNT(b) > 0 FROM Booking b
+    WHERE b.roomId = :roomId
+      AND b.checkoutTime > :oldCheckoutThreshold
+      AND b.checkinTime < :newCheckout
+""")
+    boolean existsOverlappingBooking(
+            @Param("roomId") Long roomId,
+            @Param("oldCheckoutThreshold") LocalDateTime oldCheckoutThreshold,
+            @Param("newCheckout") LocalDateTime newCheckout
+    );
+
+    @Query("""
+    SELECT COUNT(b) > 0 FROM Booking b
+    WHERE b.roomId = :roomId
+      AND b.id <> :bookingId
+      AND b.checkoutTime > :newCheckinMinus1Hour
+      AND b.checkinTime < :newCheckout
+""")
+    boolean existsOverlappingBookingExceptBookingId(
+            @Param("roomId") Long roomId,
+            @Param("newCheckinMinus1Hour") LocalDateTime newCheckinMinus1Hour,
+            @Param("newCheckout") LocalDateTime newCheckout,
+            @Param("bookingId") Long bookingId
+    );
+
+
 }
