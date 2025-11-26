ee02a8f03f40a1d3078e017df16abe1467297f2d
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-11-26 15:22:45 +0530

    add user manager page files

---
+++ frontend/src/page/user_management/UserManager.jsx
@@ -0,0 +1,11 @@
+import styles from "./UserManager.module.css";
+
+function UserManager() {
+  return (
+    <div>
+      <h1 className={styles.h1}>Hello from user manager</h1>
+    </div>
+  );
+}
+
+export default UserManager;
