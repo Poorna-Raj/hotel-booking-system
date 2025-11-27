2ae20c2e68af5c6a3481e06a08abaddaafa6a109
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-11-27 18:18:54 +0530

    replace lombok features from admin

--- user-service/src/main/java/com/hbs/user_service/data/model/Admin.java
+++ user-service/src/main/java/com/hbs/user_service/data/model/Admin.java
@@ -6,7 +6,14 @@ import lombok.Data;
 
 @Entity
 @DiscriminatorValue("ADMIN")
-@Data
 public class Admin extends User{
     private String department;
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
 }
