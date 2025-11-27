38e7dba281c323c4cacdf336920fe5587164e0f6
AroshaBandara09 <flim21038@gmail.com>
2025-11-27 16:47:46 +0530

    created booking list

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,11 @@ import { Routes, Route } from "react-router-dom";
 import BookingList from "./Component/Booking-list/BookingList";
 import AddBooking from "./Component/CRUD-booking/Add-booking/AddBooking";
 import UpdateBooking from "./Component/CRUD-booking/Update-booking/UpdateBooking";
-import ViewBooking from "./Component/CRUD-booking/View-booking/ViewBooking";
 import DeleteBookingModal from "./Component/CRUD-booking/Delete-booking/Delete-booking";
+import ViewBooking from "./page/ViewBooking/ViewBooking";
 import RoomList from "./page/RoomList/RoomList";
 import RoomDetails from "./page/RoomDetails/RoomDetails";
+
 import Login from "./page/login/Login";
 
 function App() {
@@ -17,7 +18,7 @@ function App() {
         <Route path="/BookingList" element={<BookingList />} />
         <Route path="/AddBooking" element={<AddBooking />} />
         <Route path="/Updatebooking" element={<UpdateBooking />} />
-        <Route path="/Viewbooking" element={<ViewBooking />} />
+       <Route path="/ViewBooking" element={<ViewBooking />} />
         <Route path="/Delete-booking" element={<DeleteBookingModal />} />
         <Route path="/RoomList" element={<RoomList />} />
         <Route path="/RoomDetails" element={<RoomDetails />} />
