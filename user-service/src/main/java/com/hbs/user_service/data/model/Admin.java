25c3961ab3b0b085f9d894009d05a2e90d1f38a4
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-11-27 18:20:15 +0530

    replace lombok features import from admin

--- user-service/src/main/java/com/hbs/user_service/data/model/Admin.java
+++ user-service/src/main/java/com/hbs/user_service/data/model/Admin.java
@@ -2,7 +2,6 @@ package com.hbs.user_service.data.model;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
-import lombok.Data;
 
 @Entity
 @DiscriminatorValue("ADMIN")
