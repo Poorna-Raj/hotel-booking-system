d7f8ae767fea01f32938ebd61460090d1b2f0d9c
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-11-27 18:27:27 +0530

    replace the lombok features from room dto classes

--- room-service/src/main/java/com/hbs/room_service/data/dto/RoomRequestDto.java
+++ room-service/src/main/java/com/hbs/room_service/data/dto/RoomRequestDto.java
@@ -1,10 +1,5 @@
 package com.hbs.room_service.data.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class RoomRequestDto {
     private String name;
     private String roomType;
@@ -16,4 +11,84 @@ public class RoomRequestDto {
     private String imageNo2;
     private String imageNo3;
     private String imageNo4;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public String getBedType() {
+        return bedType;
+    }
+
+    public void setBedType(String bedType) {
+        this.bedType = bedType;
+    }
+
+    public int getBedCount() {
+        return bedCount;
+    }
+
+    public void setBedCount(int bedCount) {
+        this.bedCount = bedCount;
+    }
+
+    public double getBasePrice() {
+        return basePrice;
+    }
+
+    public void setBasePrice(double basePrice) {
+        this.basePrice = basePrice;
+    }
+
+    public Long getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(Long createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getImageNo1() {
+        return imageNo1;
+    }
+
+    public void setImageNo1(String imageNo1) {
+        this.imageNo1 = imageNo1;
+    }
+
+    public String getImageNo2() {
+        return imageNo2;
+    }
+
+    public void setImageNo2(String imageNo2) {
+        this.imageNo2 = imageNo2;
+    }
+
+    public String getImageNo3() {
+        return imageNo3;
+    }
+
+    public void setImageNo3(String imageNo3) {
+        this.imageNo3 = imageNo3;
+    }
+
+    public String getImageNo4() {
+        return imageNo4;
+    }
+
+    public void setImageNo4(String imageNo4) {
+        this.imageNo4 = imageNo4;
+    }
 }
