bfa858b15d4827c90623a579868c692ba7e4364d
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-11-27 18:19:58 +0530

    replace lombok features from User

--- user-service/src/main/java/com/hbs/user_service/data/model/User.java
+++ user-service/src/main/java/com/hbs/user_service/data/model/User.java
@@ -1,13 +1,11 @@
 package com.hbs.user_service.data.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
 
 @Entity
 @Table(name = "tbl_user")
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "user_type", discriminatorType = DiscriminatorType.STRING)
-@Data
 public abstract class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,4 +18,36 @@ public abstract class User {
 
     @Column(unique = true)
     private String email;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
