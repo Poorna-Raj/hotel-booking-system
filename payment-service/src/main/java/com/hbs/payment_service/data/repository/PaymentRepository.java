cae9370d9b6928cbb7426b5ee0e31147a066b15a
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-12-11 17:20:33 +0530

    define query to get net between days

--- payment-service/src/main/java/com/hbs/payment_service/data/repository/PaymentRepository.java
+++ payment-service/src/main/java/com/hbs/payment_service/data/repository/PaymentRepository.java
@@ -1,10 +1,25 @@
 package com.hbs.payment_service.data.repository;
 
 import com.hbs.payment_service.data.model.Payment;
+import com.hbs.payment_service.data.model.PaymentStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface PaymentRepository extends JpaRepository<Payment,Long> {
     List<Payment> findByBookingId(long bookingId);
+
+    @Query("""
+    SELECT SUM(p.amount) FROM Payment p
+    WHERE p.createdAt BETWEEN :start AND :end
+    AND p.paymentStatus = :status
+    """)
+    Long getTimedRevenue(
+            @Param("start") LocalDateTime startOfWeek,
+            @Param("end") LocalDateTime endOfWeek,
+            @Param("status") PaymentStatus status
+    );
 }
