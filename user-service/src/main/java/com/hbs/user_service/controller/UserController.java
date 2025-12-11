4c94b13c88e9eb9c45075fe8145468b1df5bee60
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-12-11 15:00:30 +0530

    add validations to user request dto

--- user-service/src/main/java/com/hbs/user_service/controller/UserController.java
+++ user-service/src/main/java/com/hbs/user_service/controller/UserController.java
@@ -3,6 +3,7 @@ package com.hbs.user_service.controller;
 import com.hbs.user_service.data.dto.UserRequestDto;
 import com.hbs.user_service.data.dto.UserResponseDto;
 import com.hbs.user_service.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +16,7 @@ public class UserController {
     private UserService service;
 
     @PostMapping("/login")
-    public ResponseEntity<UserResponseDto> login(@RequestBody UserRequestDto dto){
+    public ResponseEntity<UserResponseDto> login(@Valid @RequestBody UserRequestDto dto){
         return new ResponseEntity<>(service.login(dto), HttpStatus.OK);
     }
 
