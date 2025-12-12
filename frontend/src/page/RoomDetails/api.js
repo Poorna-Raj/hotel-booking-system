9bc10d3f2e8c430d450c2a217d7803ae5e4cb92c
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-12-12 13:46:17 +0530

    define the api method for updating a room

--- frontend/src/page/RoomDetails/api.js
+++ frontend/src/page/RoomDetails/api.js
@@ -2,6 +2,13 @@ import axios from 'axios';
 
 const API_URL = 'http://localhost:8081/room-service/rooms';
 
-export const getRoomById = (id) => {
-  return axios.get(`${API_URL}/${id}`);
+export const getRoomById =
+    (id) => {
+      return axios.get(`${API_URL}/${id}`);
+    }
+
+export const updateRoomById = (id, data) => {
+  return axios.put(
+      `${API_URL}/${id}?userId=3`, data,
+      {headers: {'Content-Type': 'application/json'}});
 }
